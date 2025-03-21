ab98ff85dee8ff8268ec6c2e49e3ef0358ff8318
Elouan <elouan.tailliez@epita.fr>
2025-03-21 21:47:46 +0100

    fix: sftp test after close

--- src/sftp_connection.py
+++ src/sftp_connection.py
@@ -16,49 +16,41 @@ class SFTPConnection:
         self.sftp = None
         self.connected = False
 
-    def test_connection(self):
-        """
-        Vérifie si la connexion SFTP est active.
-        Retourne True si connecté, False sinon.
-        """
-        if self.connected and self.sftp:
-            try:
-                self.sftp.listdir(".")
-                self.connected = True
-                return True
-            except (paramiko.SSHException, IOError):
-                self.connected = False
-                return False
-        return False
-    
-    
 
+    def __del__(self):
+        self.close()
+
+    def test_connection(self):
+        try:
+            self.sftp.listdir(self.path)
+            return True
+        except Exception as e:
+            logging.error(f"Échec de la connexion SFTP : {e}")
+            return False
+    
     def connect(self):
         """
         Établit une connexion SFTP si elle n'est pas déjà active.
         """
-        if not self.test_connection():
-            try:
-                self.client.connect(self.host, self.port, self.username, self.password)
-                self.sftp = self.client.open_sftp()
-                self.connected = True
-            except Exception as e:
-                logging.error(f"Échec de la connexion SFTP : {e}")
-                self.connected = False
+        try:
+            self.client.connect(self.host, self.port, self.username, self.password)
+            self.sftp = self.client.open_sftp()
+            self.connected = True
+            return True
+        except Exception as e:
+            logging.error(f"Échec de la connexion SFTP : {e}")
+            self.connected = False
+            return False
 
     def verify_connection(self):
         """
-        Vérifie si la connexion SFTP est active.
-        Si non, tente de se connecter.
-        Retourne True si connecté, False sinon.
+        Vérifie si la connexion SFTP est active, sinon tente de la rétablir.
         """
-        if self.connected:
-            return True
-        else:
-            self.connect()
-            return self.connected
-
-
+        if not self.test_connection():
+            return self.connect()
+        self.connected = True
+        return True
+        
     def list_files(self):
         files = self.sftp.listdir(self.path)
         return files
